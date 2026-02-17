c5dcfba49ec27a06d02cc4a488c745c7a1408723
karnell schultz <KarnellSchultz@gmail.com>
2026-02-17 12:57:17 +0100

    update themes

--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -99,27 +99,22 @@ return {
     'ydkulks/cursor-dark.nvim',
     lazy = false,
     priority = 1000,
-    config = function()
-      -- vim.cmd.colorscheme("cursor-dark-midnight")
-      require('cursor-dark').setup {
-        -- For theme
-        style = 'dark-midnight',
-        -- For a transparent background
-        transparent = true,
-        -- If you have dashboard-nvim plugin installed
-        dashboard = true,
-      }
-    end,
+    -- config = function()
+    -- vim.cmd.colorscheme("cursor-dark-midnight")
+    -- require('cursor-dark').setup {}
+    -- end,
   },
   {
     'jwbaldwin/oscura.nvim',
     lazy = false,
     priority = 1000,
-    -- config = function()
-    --   require('oscura').setup {
-    --     theme = 'oscura',
-    --   }
-    --   require('oscura').load()
-    -- end,
+    config = function()
+      require('oscura').setup {
+        theme = 'oscura',
+      }
+      require('oscura').load()
+    end,
   },
+  { 'datsfilipe/vesper.nvim' },
+  { 'rebelot/kanagawa.nvim' },
 }
